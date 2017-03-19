0a055b058d2aab8eba635af65c7250c57acad704
Armando Magalhães <armando.mag95@gmail.com>
2017-03-19 01:21:10 -0300

    feat(index): Events class

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 module.exports = class Events {
-	static onServerRunning (uri) {
-		console.log(`Server running at: ${uri}`)
+	static onServerRunning(uri) {
+		console.log(`Server running at: ${uri}`);
 	}
 
-	static onServiceRegistered () {
-		console.log("Server registered for Service Discovery.")
+	static onServiceRegistered() {
+		console.log('Server registered for Service Discovery.');
 	}
 
-	static onServiceUnregistered () {
-		console.log("Server unregistered from Service Discovery.")
+	static onServiceUnregistered() {
+		console.log('Server unregistered from Service Discovery.');
 	}
 
-	static onServiceRegisterError (err) {
-		console.error("Can't register for Service Discovery.")
+	static onServiceRegisterError(err) {
+		console.error('Can\'t register for Service Discovery.');
+		console.log('Cause:', err);
 	}
 
-	static onServiceUnregisterError (err) {
-		console.error("Can't unregister from Service Discovery.")
+	static onServiceUnregisterError(err) {
+		console.error('Can\'t unregister from Service Discovery.');
+		console.log('Cause:', err);
 	}
-}
+};
