0bc93ba578c398a6c40f0c73cca894cca8f2750e
Armando Magalhães <armando.mag95@gmail.com>
2017-03-19 01:10:38 -0300

    test(index): testing exposed functions

--- test.js
+++ test.js
@@ -1,2 +1,10 @@
 import test from 'ava';
 import Events from '.';
+
+test('Events', t => {
+	t.true((typeof Events.onServerRunning === 'function'), 'Events.onServerRunning is not a function.');
+	t.true((typeof Events.onServiceRegistered === 'function'), 'Events.onServiceRegistered is not a function.');
+	t.true((typeof Events.onServiceUnregistered === 'function'), 'Events.onServiceUnregistered is not a function.');
+	t.true((typeof Events.onServiceRegisterError === 'function'), 'Events.onServiceRegisterError is not a function.');
+	t.true((typeof Events.onServiceUnregisterError === 'function'), 'Events.onServiceUnregisterError is not a function.');
+});
